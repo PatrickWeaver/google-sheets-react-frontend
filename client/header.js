595fd7cc4a3c1c90f096042b89e0483b2bc8a36d
Glitch (google-sheet-react-frontend) <none>
2018-03-11 21:25:08 +0000

    Working routing with react-router

--- client/header.js
+++ client/header.js
@@ -1,16 +1,16 @@
 const Header = function(props) {
   
-  const pages = props.pages.map(page => 
-    <li><h3><a href={"/" + page.title}>{page.title}</a></h3></li>                       
+  const pages = props.pages.map(page =>   
+    <li><h3><Link to={"/" + page.title}>{page.title}</Link></h3></li>
   );
   
   
   return (
-    <div id="header">
-      <h1>Google Sheets App: React Website</h1>
-      <ul id="menu">
-        {pages}
-      </ul>
-    </div>
+      <div id="header">
+        <h1>Google Sheets App: React Website</h1>
+        <ul id="menu">
+          {pages}
+        </ul>
+      </div>
   ); 
 }
