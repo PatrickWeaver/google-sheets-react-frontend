50cf594925529120e4388717798579c67de9e06b
Glitch (google-sheet-react-website) <none>
2018-03-12 15:45:05 +0000

    Only show menu if there is more than one sheet

--- client/header.js
+++ client/header.js
@@ -1,9 +1,27 @@
 const Header = function(props) {
+  /*
+    const imageArray = Object.entries(props.row).reduce((accumulator, current) => {
+      if (current[1].image) {
+        var image = current[1].url;
+        accumulator.push(image);
+      }
+      return accumulator;
+    }, []);
+    
+    imageList = imageArray.map(image =>
+      <div class="image"><img src={image} /></div>
+    );
   
-  const pages = props.pages.map(page =>   
-    <li><h3><Link to={"/" + page.title}>{page.title}</Link></h3></li>
-  );
   
+  
+  
+  */
+  var pages = null;
+  if (props.pages.length > 1) { 
+    var pages = props.pages.map(page =>   
+      <li><h3><Link to={"/" + page.title}>{page.title}</Link></h3></li>
+    );
+  }
   
   return (
       <div id="header">
